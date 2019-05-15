d63f24654845b33e4493f7914df95d724a10c817
specialblend <cj@specialblend.net>
2019-05-15 14:19:44 -0500

    add test for package.json

--- exports.test.js
+++ exports.test.js
@@ -1,9 +1,18 @@
 const pkg = require('./dist/index');
+const packageJSON = require('./package.json');
 
 const functions = ['injectPayload', 'injectScript', 'renderScript', 'resolveEnv'];
 
 describe('exports are importable', () => {
     test.each(functions)('%p', func => {
         expect(pkg[func]).toBeFunction();
+    });
+});
+
+describe('package.json has correct exports', () => {
+    expect(packageJSON).toMatchObject({
+        main: 'src/index.js',
+        module: 'dist/index.js',
+        browser: 'dist/index.js',
     });
 });
