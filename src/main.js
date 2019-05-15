e0f2c9aa8442340c4bead0e8deda7f0f54075ad6
specialblend <cj@specialblend.net>
2019-05-15 00:33:00 -0500

    use express-interceptor

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
+import interceptor from 'express-interceptor';
 import { renderPage } from './common';
 
-export default payload =>
-    (req, res) => {
-        res.send(renderPage(payload, res.body.toString()));
-    };
+export default payload => interceptor(
+    (request, response) => ({
+        // Only intercept HTML files
+        isInterceptable() {
+            return /text\/html/.test(response.get('Content-Type'));
+        },
+        // Inject <script> tag
+        intercept(body, send) {
+            send(renderPage(payload, body));
+        },
+    })
+);
