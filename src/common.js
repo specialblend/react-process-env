7ef513b5eebf1ef22fb6e6716d4353c2fa1d4050
specialblend <cj@specialblend.net>
2019-05-15 14:40:54 -0500

    housekeeping

--- src/common.js
+++ src/common.js
@@ -1,4 +1,30 @@
-export const toBase64 = data => Buffer.from(data).toString('base64');
-export const fromBase64 = data => Buffer.from(data, 'base64').toString('ascii');
-export const encodeData = data => toBase64(JSON.stringify(data));
-export const decodeData = data => JSON.parse(fromBase64(data));
+import * as R from 'ramda';
+
+/**
+ * Encode payload into base64
+ * @param {Object} payload: payload
+ * @return {string}: base64 payload
+ */
+export const toBase64 = payload => Buffer.from(payload).toString('base64');
+
+/**
+ * Decode payload from base64 to ascii
+ * @param {Object} payload: payload
+ * @return {string}: ascii payload
+ */
+export const fromBase64 = payload => Buffer.from(payload, 'base64').toString('ascii');
+
+/**
+ * Stringify and encode payload into base64
+ * @param {Object} payload: payload
+ * @return {string}: encoded payload
+ */
+export const encodeData = payload => toBase64(JSON.stringify(payload));
+
+/**
+ * Decode and parse payload from base64
+ * @param {Object} payload: payload
+ * @return {string}: decoded payload
+ */
+export const decodeData = payload => JSON.parse(fromBase64(payload));
+
--- src/inject.js
+++ src/inject.js
@@ -3,6 +3,19 @@ import { encodeData } from './common';
 
 export const renderScript = data => `<script>window.env=JSON.parse(atob('${encodeData(data)}'))</script>`;
 
+/**
+ * Render payload into <script> tag
+ * @param {Object} payload: payload
+ * @return {String}: <script> tag
+ */
+export const renderScript = payload => `<script>window.env=JSON.parse(atob('${encodeData(payload)}'))</script>`;
+
+/**
+ * Inject rendered script tag into <head> of HTML body
+ * @param {String} body: HTML body
+ * @param {Object} payload: payload
+ * @return {String}: HTML body with <script> tag
+ */
 export const injectScript = (body, payload) => {
     const script = renderScript(payload);
     const $ = cheerio.load(body);
@@ -10,6 +23,12 @@ export const injectScript = (body, payload) => {
     return $.html();
 };
 
+/**
+ * Create express callback that injects script into resolved HTML body
+ * @param {Object} payload: payload
+ * @param {Function} resolver: async callback to resolve the HTML body
+ * @return {function(*, *): (boolean | void)}: express callback
+ */
 export const injectPayload = (payload, resolver) =>
     async(req, res) => {
         const body = await resolver();
