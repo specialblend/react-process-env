0d7543b0601c05fc734e4b8a0a42b232b69b6db7
specialblend <cj@specialblend.net>
2019-05-15 17:35:30 -0500

    fix docs commit empty

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-cat support/README.md > README.md && \
-jsdoc2md dist/index.esm.js >> README.md && \
-git add README.md && \
-git add support/ && \
-git commit -m "update docs"
+cat support/README.md > README.md && jsdoc2md dist/index.esm.js >> README.md
+git add README.md
+git add support
+git diff-index --quiet HEAD || git commit -m "update docs"
