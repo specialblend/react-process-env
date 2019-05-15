4a64fd678baa2d295c078fbe9229b09e7d226ef1
specialblend <cj@specialblend.net>
2019-05-15 18:45:37 -0500

    resolve process.env before window.env

--- src/resolve.js
+++ src/resolve.js
@@ -1,12 +1,12 @@
 import * as R from 'ramda';
 
 /**
- * Resolve property from `window.env` (`express`/production), or fallback to `process.env` (`react-scripts`/development)
+ * Resolve property from `process.env` (`react-scripts`/development), or `window.env` (`express`/production)
  * @param {string} prop name of property to resolve
  * @return {*} value of the resolved property
  */
 export const resolveEnv = prop =>
     R.either(
-        R.path(['window', 'env', prop]),
         R.path(['process', 'env', prop]),
+        R.path(['window', 'env', prop]),
     )({ process, window: global });
--- src/resolve.test.js
+++ src/resolve.test.js
@@ -4,17 +4,17 @@ import { resolveEnv } from './resolve';
 global.env = {};
 
 describe('pulls expected prop', () => {
-    test('when window.env[prop] is set', () => {
-        global.env.__test__qwertyuiop = 'test_value_window';
-        process.env.__test__qwertyuiop = 'test_value_process';
-        expect(resolveEnv('__test__qwertyuiop')).toBe('test_value_window');
-    });
-    test('when window.env[prop] is not set', () => {
+    test('when process.env[prop] is set, and window.env[prop] is set', () => {
         global.env = {};
         process.env.__test__zxcvbnm = 'test_value_process';
+        global.env.__test__zxcvbnm = 'test_value_window';
         expect(resolveEnv('__test__zxcvbnm')).toBe('test_value_process');
     });
-    test('when window.env is not set', () => {
+    test('when process.env[prop] is not set, but window.env[prop] is set', () => {
+        global.env.__test__qwertyuiop = 'test_value_window';
+        expect(resolveEnv('__test__qwertyuiop')).toBe('test_value_window');
+    });
+    test('when process.env[prop] is set, and window.env[prop] is not set', () => {
         Reflect.deleteProperty(global, 'env');
         process.env.__test__asdfg = 'test_value_process';
         expect(resolveEnv('__test__asdfg')).toBe('test_value_process');
