cb9536aac6223a2b7e5241277d71a8c7047afedb
CJ Salem <cj.salem@fishtech.group>
2019-05-16 14:26:02 -0500

    resolve process.env in default args

--- src/resolve.js
+++ src/resolve.js
@@ -1,21 +1,10 @@
 import * as R from 'ramda';
 
 /**
- * Resolve the current global process and window
- * @return {Object} context
- */
-const resolveSuperContext = () => ({
-    process: {
-        env: process.env,
-    },
-    window: global,
-});
-
-/**
  * Resolve property from `process.env` (`react-scripts`/development), or `window.env` (`express`/production)
  * @param {string} prop name of property to resolve
  * @param {object} context an object containing process and window globals
  * @return {*} value of the resolved property
  */
-export const resolveEnv = (prop, context = resolveSuperContext()) =>
+export const resolveEnv = (prop, context = { process: { env: process.env }, window: global }) =>
     R.defaultTo(null, R.either(R.path(['process', 'env', prop]), R.path(['window', 'env', prop]))(context));
