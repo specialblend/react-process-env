732f3b8ceea1b5b72f4d12aa2a7e5ff72219d752
specialblend <cj@specialblend.net>
2019-05-15 19:05:49 -0500

    allow passing context

--- src/resolve.js
+++ src/resolve.js
@@ -3,10 +3,11 @@ import * as R from 'ramda';
 /**
  * Resolve property from `process.env` (`react-scripts`/development), or `window.env` (`express`/production)
  * @param {string} prop name of property to resolve
+ * @param {object} context an object containing process and window globals
  * @return {*} value of the resolved property
  */
-export const resolveEnv = prop =>
+export const resolveEnv = (prop, context = { process, window: global }) =>
     R.either(
         R.path(['process', 'env', prop]),
         R.path(['window', 'env', prop]),
-    )({ process, window: global });
+    )(context);
--- src/resolve.test.js
+++ src/resolve.test.js
@@ -1,25 +1,56 @@
 /* eslint-disable camelcase */
 import { resolveEnv } from './resolve';
 
-global.env = {};
+const window = global;
+window.env = {};
 
 describe('pulls expected prop', () => {
-    test('when process.env[prop] is set, and window.env[prop] is set', () => {
-        global.env = {};
-        process.env.__test__zxcvbnm = 'test_value_process';
-        global.env.__test__zxcvbnm = 'test_value_window';
-        expect(resolveEnv('__test__zxcvbnm')).toBe('test_value_process');
+    describe('with default context', () => {
+        test('when process.env[prop] is set, and window.env[prop] is set', () => {
+            window.env = {};
+            process.env.__test__zxcvbnm = 'test_value_process';
+            window.env.__test__zxcvbnm = 'test_value_window';
+            expect(resolveEnv('__test__zxcvbnm')).toBe('test_value_process');
+        });
+        test('when process.env[prop] is not set, but window.env[prop] is set', () => {
+            window.env.__test__qwertyuiop = 'test_value_window';
+            expect(resolveEnv('__test__qwertyuiop')).toBe('test_value_window');
+        });
+        test('when process.env[prop] is set, and window.env[prop] is not set', () => {
+            Reflect.deleteProperty(window, 'env');
+            process.env.__test__asdfg = 'test_value_process';
+            expect(resolveEnv('__test__asdfg')).toBe('test_value_process');
+        });
+        test('when neither window.env[prop] or process.env[prop] is set', () => {
+            expect(resolveEnv('__test__qawsedrftgyhuji')).toBeUndefined();
+        });
     });
-    test('when process.env[prop] is not set, but window.env[prop] is set', () => {
-        global.env.__test__qwertyuiop = 'test_value_window';
-        expect(resolveEnv('__test__qwertyuiop')).toBe('test_value_window');
-    });
-    test('when process.env[prop] is set, and window.env[prop] is not set', () => {
-        Reflect.deleteProperty(global, 'env');
-        process.env.__test__asdfg = 'test_value_process';
-        expect(resolveEnv('__test__asdfg')).toBe('test_value_process');
-    });
-    test('when neither window.env[prop] or process.env[prop] is set', () => {
-        expect(resolveEnv('__test__qawsedrftgyhuji')).toBeUndefined();
+    describe('when passing context', () => {
+        const context = {
+            window: {
+                env: {},
+            },
+            process: {
+                env: {},
+            },
+        };
+        test('when process.env[prop] is set, and window.env[prop] is set', () => {
+            context.window.env = {};
+            context.process.env.__test__zxcvbnm = 'test_value_process';
+            context.window.env.__test__zxcvbnm = 'test_value_window';
+            expect(resolveEnv('__test__zxcvbnm', context)).toBe('test_value_process');
+        });
+        test('when process.env[prop] is not set, but window.env[prop] is set', () => {
+            context.window.env.__test__qwertyuiop = 'test_value_window';
+            expect(resolveEnv('__test__qwertyuiop', context)).toBe('test_value_window');
+        });
+        test('when process.env[prop] is set, and window.env[prop] is not set', () => {
+            Reflect.deleteProperty(context.window, 'env');
+            context.process.env.__test__asdfg = 'test_value_process';
+            expect(resolveEnv('__test__asdfg', context)).toBe('test_value_process');
+        });
+        test('when neither window.env[prop] or process.env[prop] is set', () => {
+            expect(resolveEnv('__test__qawsedrftgyhuji', context)).toBeUndefined();
+        });
     });
 });
