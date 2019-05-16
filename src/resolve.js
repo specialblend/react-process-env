5c69b9a658b8a8b6e0fddaa9f57857f6dbfa97ce
CJ Salem <cj.salem@fishtech.group>
2019-05-16 14:29:54 -0500

    refactor to plain js

--- src/resolve.js
+++ src/resolve.js
@@ -1,10 +1,24 @@
-import * as R from 'ramda';
-
 /**
  * Resolve property from `process.env` (`react-scripts`/development), or `window.env` (`express`/production)
  * @param {string} prop name of property to resolve
  * @param {object} context an object containing process and window globals
  * @return {*} value of the resolved property
  */
-export const resolveEnv = (prop, context = { process: { env: process.env }, window: global }) =>
-    R.defaultTo(null, R.either(R.path(['process', 'env', prop]), R.path(['window', 'env', prop]))(context));
+export const resolveEnv = (prop, context = { process: { env: process.env }, window: global }) => {
+    if (context.process) {
+        const processValue = context.process.env[prop];
+        if (processValue) {
+            return processValue;
+        }
+    }
+    if (context.window) {
+        if (context.window.env) {
+            const windowValue = context.window.env[prop];
+            if (windowValue) {
+                return windowValue;
+            }
+        }
+    }
+};
+// export const resolveEnv = (prop, context = { process: { env: process.env }, window: global }) =>
+//     R.defaultTo(null, R.either(R.path(['process', 'env', prop]), R.path(['window', 'env', prop]))(context));
