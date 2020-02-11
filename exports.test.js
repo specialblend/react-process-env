e5df07141713c353fcfc2c6051f36bc16038f070
specialblend <cj@specialblend.net>
2020-02-10 18:15:29 -0600

    test require default

--- exports.test.js
+++ exports.test.js
@@ -1,13 +1,26 @@
 import _inject from './lib';
-import * as _module from './lib';
+import * as _imported from './lib';
 const functions = ['injectScript', 'renderScript', 'resolve'];
 
-describe('compiled module', () => {
+const _required = require('./lib');
+
+describe('imported module', () => {
     test('exports default function', () => {
         expect(_inject).toBeFunction();
 
     });
     test.each(functions)('exports function %p', funcName => {
-        expect(_module[funcName]).toBeFunction();
+        expect(_imported[funcName]).toBeFunction();
+    });
+});
+
+
+describe('_required module', () => {
+    test('exports default function', () => {
+        expect(_required.default).toBeFunction();
+
+    });
+    test.each(functions)('exports function %p', funcName => {
+        expect(_required[funcName]).toBeFunction();
     });
 });
